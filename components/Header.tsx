5768e6679e4f345c8e841664a2e71d4a849b7c8d
Samuel Guerrero <itougn15@gmail.com>
2022-09-04 12:48:36 -0500

    Init project

---
+++ components/Header.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+import { BeakerIcon, ChevronDownIcon, HomeIcon } from "@heroicons/react/solid";
+import { StarIcon } from "@heroicons/react/outline";
+
+function Header() {
+  return (
+    <div>
+      <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
+        <Image
+          objectFit="contain"
+          src="https://links.papareact.com/fqy"
+          layout="fill"
+        />
+      </div>
+
+      <div>
+        <HomeIcon />
+        <p>Home</p>
+        <ChevronDownIcon />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
