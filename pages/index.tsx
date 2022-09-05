e55714685eec074f69f5ec0bc2da40700d8ac95d
Samuel Guerrero <itougn15@gmail.com>
2022-09-04 22:44:03 -0500

    More configurations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
+import PostBox from "../components/PostBox";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-5xl my-7 mx-auto">
       <Head>
         <title>Reddit 2.0 Clone</title>
       </Head>
 
-      <Header />
+      {/* PostBox */}
+      <PostBox />
+
+      <div className="flex">{/* Feed */}</div>
     </div>
   );
 };
